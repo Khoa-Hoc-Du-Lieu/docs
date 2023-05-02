3be061ee4927a1e03b9283a998b299b92eb0bf20
Nguyenkhang.me <duncannguyen2k@gmail.com>
2023-05-02 21:52:27 +0700

    feat: add community section

--- src/components/homepage/CommunitySection.jsx
+++ src/components/homepage/CommunitySection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
 
-import { Linkedin, Twitter } from 'react-feather';
-import { DiscordIcon } from '@site/src/icons';
+import { GitHub } from 'react-feather';
+import { DiscordIcon, TelegramIcon } from '@site/src/icons';
 
 const githubUsernames = [
   'nguyenkhangme',
@@ -15,11 +15,10 @@ export default function CommunitySection() {
     <section className="no-underline-links">
       <div className="mx-auto flex w-full flex-col items-center justify-center bg-gradient-to-b from-zinc-200/90 to-white px-4 py-16 pt-64 text-zinc dark:from-[#262626] dark:to-black dark:text-white">
         <h2 className="text-3xl">
-          Join the <span className="text-primary-100">community</span>
+          Tham gia <span className="text-primary-100">cộng đồng</span>
         </h2>
         <p className="mb-10 text-zinc-500">
-          Engage with our ever-growing community to get the latest updates,
-          product support, and more.
+          Tham gia với cộng đồng Khoahocdulieu.org để cùng chia sẻ đam mê, nhận hỗ trợ, hợp tác nghiên cứu,...
         </p>
         <div className="mx-auto mb-16 flex flex-wrap -space-x-1.5">
           {githubUsernames.map((username) => (
@@ -35,21 +34,21 @@ export default function CommunitySection() {
         <div className="flex w-full flex-col items-center justify-center gap-2 text-sm font-semibold lg:flex-row lg:gap-8">
           <Link
             className="flex w-full items-center justify-center gap-2 rounded-sm border border-solid border-primary-100 bg-primary-100/10 px-2 py-2 text-primary-100 lg:w-auto"
-            href="https://community.dyte.io"
+            href="https://discord.gg/QnkvBEJ3kn"
           >
             <DiscordIcon className="h-6 w-6" /> Discord &rarr;
           </Link>
           <Link
             className="flex w-full items-center justify-center gap-2 rounded-sm border border-solid border-primary-100 bg-primary-100/10 px-2 py-2 text-primary-100 lg:w-auto"
-            href="https://twitter.com/dyte_io"
+            href="https://github.com/Khoa-Hoc-Du-Lieu/docs/discussions"
           >
-            <Twitter className="h-5 w-5" /> Twitter &rarr;
+            <GitHub className="h-5 w-5" /> GitHub &rarr;
           </Link>
           <Link
             className="flex w-full items-center justify-center gap-2 rounded-sm border border-solid border-primary-100 bg-primary-100/10 px-2 py-2 text-primary-100 lg:w-auto"
-            href="https://www.linkedin.com/company/dyteio"
+            href="https://t.me/khoahocdulieu"
           >
-            <Linkedin className="h-5 w-5" /> LinkedIn &rarr;
+            <TelegramIcon className="h-5 w-5" /> Telegram &rarr;
           </Link>
         </div>
       </div>
